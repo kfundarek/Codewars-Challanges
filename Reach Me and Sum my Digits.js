de9ae80c3c58d0847d0dc8794c6bf46844b6e49d
Kevin Fundarek <kfundarek@gmail.com>
2016-11-23 22:13:45 -0500

    Add files via upload

--- Reach Me and Sum my Digits.js
+++ Reach Me and Sum my Digits.js
@@ -1,6 +1,23 @@
 //Reach Me and Sum my Digits
 function sumDigNthTerm(initval, patternl, nthterm) {
     var holderArr = [initval];
+    var placeHolder = initval;
+    while(holderArr.length<=nthterm){
+      	for (var i = 0, j = placeHolder; i<patternl.length; i++){
+	  		j += patternl[i];
+	  		holderArr.push(j);
+	  		if (i === patternl.length-1){
+	  		  placeHolder = j;
+	  		}
+	  	  }
+    }
+    var rawAns = holderArr[nthterm-1];
+    return rawAns.toString().split('').reduce(function(accum, item){return parseInt(accum)+parseInt(item)})
+}
+
+//A recursive solution
+function sumDigNthTerm(initval, patternl, nthterm) {
+    var holderArr = [initval];
     var subRoutine = function (val) {
 	    if(holderArr.length >= nthterm){
 	      var rawAns = holderArr[nthterm-1];
