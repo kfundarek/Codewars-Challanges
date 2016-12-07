fd67f7ec65d3c4402973655fd36c2c68f78a3416
Kevin Fundarek <kfundarek@gmail.com>
2016-12-07 13:51:48 -0500

    Add files via upload

--- Sum of Pairs.js
+++ Sum of Pairs.js
@@ -1,22 +1,24 @@
 //Sum of Pairs
-//Initial solution passes all tests, but needs optimization
 var sum_pairs=function(ints, s){
 	var retIndArr = [undefined, ints[ints.length-1]];
 	retArr = [];
+	var cash = {}
 	var x = ints.lastIndexOf(retIndArr[1])
 	for (var i = 0; i < x; i++){
-	  for(var j = i+1; j <= x; j++){
-	    if (ints[i]+ints[j] === s){
-	      retIndArr[0]=i;
-	      retIndArr[1]=j;
-	      x = j
-	      retArr[0]=ints[i];
-	      retArr[1]=ints[j];
-	      break;
+	  if (!cash.hasOwnProperty(ints[i])){
+	  	cash[ints[i]] = ints[i];
+		for(var j = i+1; j <= x; j++){
+		  if (ints[i]+ints[j] === s){
+		    retIndArr[0]=i;
+		    retIndArr[1]=j;
+		    x = j
+            retArr[0]=ints[i];
+    	    retArr[1]=ints[j];
+		    break;
+	      }
 	    }
 	  }
 	}
-
 	if (retArr[0] === undefined){
 	  return undefined
 	}
