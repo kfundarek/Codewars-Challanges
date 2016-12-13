f9ee7abb42a86a07a2ae7a07f82eb2f076c7cca3
Kevin Fundarek <kfundarek@gmail.com>
2016-12-12 20:44:34 -0500

    Add files via upload

---
+++ Maximum subarray sum.js
@@ -0,0 +1,27 @@
+//Maximum subarray sum
+//Brute force solution in need of optimizaton
+var maxSequence = function(arr){
+  // ...
+  var currentHigh = 0;
+  var previousHigh = 0;
+  var currentCount = 0;
+  var allNeg = arr.reduce(function(accum, item){
+  	if (item > 0){
+  		accum = false
+  	}
+  }, true)
+  if (allNeg || arr === []){
+  	return 0
+  }
+
+  for (let i=0; i<arr.length; i++){
+  	for (let j = i; j<arr.length; j++){
+  		currentCount += arr[j]
+  		if(currentCount>currentHigh){
+  			currentHigh = currentCount
+  		}
+    }
+    currentCount=0;
+  };
+  return currentHigh
+}
