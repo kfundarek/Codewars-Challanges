aee0667d485351e2de846cc0147bd03e244ef33f
Kevin Fundarek <kfundarek@gmail.com>
2016-12-20 20:39:43 -0500

    Update Primes in numbers.js

--- Primes in numbers.js
+++ Primes in numbers.js
@@ -16,59 +16,85 @@ function primeFactors(n){
        }                                
      }                                  
   }
-  console.log(primeArr);
   }
 
-  	getPrimes(primeGenLimit);  
+   var isPrime = function(n){
+//    """Returns True if n is prime."""
+    if (n === 2){
+        return true
+    }
+    if (n === 3){
+        return true
+    }
+    if (n % 2 === 0){
+        return false
+    }
+    if (n % 3 === 0){
+        return false
+    }
+
+    i = 5
+    w = 2
+
+    while (i * i <= n){
+        if (n % i === 0){
+            return false
+        }
+        i += w
+        w = 6 - w
+    }
+    return true
+ }
+    if (isPrime(n)){
+      return `(${n})`
+    };
+    
+    getPrimes(primeGenLimit);  
     //check if n is prime against the PNG cache
     if (primeArr.includes(n)){
-    	return `(${n})`
+      return `(${n})`
     }
     
     //try to divide n by increasing primes given by the PNG untill the n is divided without a remainder
     var subRT = function(n){
     for (let i = 0; i<primeArr.length; i++){
-    	if (n%primeArr[i] === 0 && primeArr.includes(n/primeArr[i])){
-    		primeDivisors.push(primeArr[i]);
-    		primeDivisors.push(n/primeArr[i]);
-    		break;
-    	}
-    	if (i>=primeArr.length-1){
-    		primeGenLimit += 100;
-    		getPrimes(primeGenLimit);
-    		if (primeArr.includes(n)){
-    	      return `(${n})`
+      if (n%primeArr[i] === 0 && isPrime(n/primeArr[i])){
+        primeDivisors.push(primeArr[i]);
+        primeDivisors.push(n/primeArr[i]);
+        break;
+      }
+      if (i>=primeArr.length-1){
+        primeGenLimit += 100;
+        getPrimes(primeGenLimit);
+        if (primeArr.includes(n)){
+            return `(${n})`
             }
-    	}
-    	if (n%primeArr[i] === 0 && !primeArr.includes(n/primeArr[i])){
-    		primeDivisors.push(primeArr[i])
-    		console.log(`the Divisor is ${primeArr[i]}`)
-    		n = n/primeArr[i];
-    		console.log(`the new dividend is ${n}`)
-    		console.log(`the retArr is ${primeDivisors}`)
-
-    		subRT(n)
-    		break;
-    	}
+      }
+      if (n%primeArr[i] === 0 && !isPrime(n/primeArr[i])){
+        primeDivisors.push(primeArr[i])
+        n = n/primeArr[i];
+        subRT(n)
+        break;
+      }
     }  
     }
+    
     subRT(n)
     var retArr = primeDivisors.map(function(item, index){
-    	var countArr= primeDivisors.filter(function(element){return element === item})
-    	if(countArr.length>1){
-    		return `(${item}**${countArr.length})`
-    	}
-    	else{
-    		return `(${item})`
-    	}
+      var countArr= primeDivisors.filter(function(element){return element === item})
+      if(countArr.length>1){
+        return `(${item}**${countArr.length})`
+      }
+      else{
+        return `(${item})`
+      }
     })
 
     var finalRetArr =[]
     retArr.forEach(function(item){
-    	if(!finalRetArr.includes(item)){
-    		finalRetArr.push(item)
-    	}
-	})
+      if(!finalRetArr.includes(item)){
+        finalRetArr.push(item)
+      }
+  })
     return finalRetArr.join('')
 }
-primeFactors(10);
