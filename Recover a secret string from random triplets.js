81bcfabcbbea80a299e88f93aa6c1485bbdd5531
Kevin Fundarek <kfundarek@gmail.com>
2016-12-27 16:03:28 -0500

    Add files via upload

--- Recover a secret string from random triplets.js
+++ Recover a secret string from random triplets.js
@@ -1,5 +1,4 @@
 //Recover a secret string from random triplets
-//Working solution that needs to be optimized: 
 var recoverSecret = function(triplets) {
 	function filterDuplicates(arr){
 		var tempArr=[];
@@ -10,46 +9,45 @@ var recoverSecret = function(triplets) {
 		})
 		return tempArr;
 	}
-
-	var workingObj = {};
-	var finalObj ={};
 	var retArr = [];
-	var letterCount = 0;
-	triplets.forEach(function(element){
-		element.forEach(function(item){
-			if(!workingObj.hasOwnProperty(item)){
-				workingObj[item]=[];    
-	    	}
-	  	})
-		workingObj[element[0]].push(element[1]);
-		workingObj[element[0]].push(element[2]);
-		workingObj[element[1]].push(element[2]);
-	})
-	
-	
-	for(let key in workingObj){
-		workingObj[key].forEach(function(item){
-			workingObj[key]= workingObj[key].concat(workingObj[item]);
-		})
+	for (let i = 0; i<triplets.length; i++){
+		retArr = retArr.concat(triplets[i])
+	}
+	retArr = filterDuplicates(retArr);
+
+	for (let i=0; i<triplets.length; i++){
+			//for element 0:
+				if (retArr.indexOf(triplets[i][0]) > retArr.indexOf(triplets[i][1])){
+					retArr.splice(retArr.indexOf(triplets[i][0]), 1);
+					retArr.splice(retArr.indexOf(triplets[i][1])-1, 0, triplets[i][0])
+				}
+				if (retArr.indexOf(triplets[i][0]) > retArr.indexOf(triplets[i][2])){
+					retArr.splice(retArr.indexOf(triplets[i][0]), 1);
+					retArr.splice(retArr.indexOf(triplets[i][2])-1, 0, triplets[i][0])
+				}
+			//for element 1
+				if (retArr.indexOf(triplets[i][1]) > retArr.indexOf(triplets[i][2])){
+					retArr.splice(retArr.indexOf(triplets[i][1]), 1);
+					retArr.splice(retArr.indexOf(triplets[i][2])-1, 0, triplets[i][1])
+				}
 	}
 	
-	for(let key in workingObj){
-		workingObj[key].forEach(function(item){
-			workingObj[key]= workingObj[key].concat(workingObj[item]);
-		})
+	for (let i=0; i<triplets.length; i++){
+			//for element 2:
+				if (retArr.indexOf(triplets[i][2]) < retArr.indexOf(triplets[i][1])){
+					retArr.splice(retArr.indexOf(triplets[i][2]), 1);
+					retArr.splice(retArr.indexOf(triplets[i][1])+1, 0, triplets[i][2])
+				}
+				if (retArr.indexOf(triplets[i][2]) < retArr.indexOf(triplets[i][0])){
+					retArr.splice(retArr.indexOf(triplets[i][2]), 1);
+					retArr.splice(retArr.indexOf(triplets[i][0])-1, 0, triplets[i][2])
+				}
+			//for element 1
+				if (retArr.indexOf(triplets[i][1]) < retArr.indexOf(triplets[i][0])){
+					retArr.splice(retArr.indexOf(triplets[i][1]), 1);
+					retArr.splice(retArr.indexOf(triplets[i][0])+1, 0, triplets[i][1])
+				}
 	}
 	
-	
-	for(let key in workingObj){
-		letterCount++
-		workingObj[key]=filterDuplicates(workingObj[key]);
-	}
-	for(var i=0; i<=letterCount; i++){
-		for(let key in workingObj){
-			if(workingObj[key].length===i){
-				retArr.unshift(key)
-			}
-		}
-	}
 	return retArr.join('');
 }
